88470cd99c7c479b40abdbe489c97155415d277d
Dylan Roten <Dylanroten@gmail.com>
2019-06-30 15:32:11 -0400

    assassino can move!!!!

---
+++ Assets/Scripts/CameraMobility (1).cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMobility : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// LateUpdate is called once per frame after update
+	void LateUpdate () {
+        transform.position = new Vector3(player.transform.position.x + 3, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour {
+    
+    public int speed;
+    public Sprite[] sprites;
+
+	private void Awake()
+	{
+        //sprites = Resources.LoadAll<Sprite>("Sprites");
+	}
+
+	private void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += new Vector3(-.01f*speed, 0, 0);
+                    }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += new Vector3(0.01f*speed, 0, 0);
+            
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0.01f*speed);
+            
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -0.01f*speed);
+
+        }
+        else 
+        {
+            //GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+	}
+
+}
